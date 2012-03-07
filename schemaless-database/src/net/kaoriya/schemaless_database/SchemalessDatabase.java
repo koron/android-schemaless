2a997bcb783ab21a2ca794129ce93688c1491829
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-03-08 08:31:18 +0900

    implement database abstract operation

--- schemaless-database/src/net/kaoriya/schemaless_database/SchemalessDatabase.java
+++ schemaless-database/src/net/kaoriya/schemaless_database/SchemalessDatabase.java
@@ -1,5 +1,8 @@
 package net.kaoriya.schemaless_database;
 
+import java.util.Map;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -26,6 +29,12 @@ public final class SchemalessDatabase implements Schemaless {
             }
         };
     }
+
+    public void close() {
+        this.openHelper.close();
+    }
+
+    ////////////////////////////////////////////////////////////////////////
 
     /**
      * Setup schema.
@@ -61,8 +70,148 @@ public final class SchemalessDatabase implements Schemaless {
                 "  ON propvals (fld_id)");
     }
 
-    public void close() {
-        this.openHelper.close();
+    private static long insertRecord(SQLiteDatabase db, ContentValues cv)
+    {
+        db.beginTransaction();
+        try {
+            long now = System.currentTimeMillis();
+            long recId = insertRechdr(db, now);
+            if (recId > 0) {
+                insertPropval(db, recId, cv, now);
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+        return 0;
     }
+
+    private static boolean updateRecord(
+            SQLiteDatabase db,
+            long recId,
+            ContentValues cv)
+    {
+        db.beginTransaction();
+        try {
+            long now = System.currentTimeMillis();
+            if (updateRechdr(db, recId, now)) {
+                updatePropval(db, recId, cv, now);
+                db.setTransactionSuccessful();
+            }
+        } finally {
+            db.endTransaction();
+        }
+        return false;
+    }
+
+    private static void deleteRecord(SQLiteDatabase db, long recId)
+    {
+        db.beginTransaction();
+        try {
+            deleteRechdr(db, recId);
+            deletePropval(db, recId);
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    private static ContentValues getRecord(SQLiteDatabase db, long recId)
+    {
+        // TODO:
+        return null;
+    }
+
+    private static long getField(SQLiteDatabase db, String name)
+    {
+        // TODO:
+        return 0;
+    }
+
+    private static long assureField(SQLiteDatabase db, String name)
+    {
+        // TODO:
+        return 0;
+    }
+
+    private static long insertRechdr(SQLiteDatabase db, long time)
+    {
+        // TODO:
+        return 0;
+    }
+
+    private static boolean updateRechdr(
+            SQLiteDatabase db,
+            long recId,
+            long time)
+    {
+        // TODO:
+        return false;
+    }
+
+    private static void deleteRechdr(SQLiteDatabase db, long recId)
+    {
+        // TODO:
+    }
+
+    private static void insertPropval(
+            SQLiteDatabase db,
+            long recId,
+            ContentValues cv,
+            long now)
+    {
+        for (Map.Entry<String,Object> value : cv.valueSet()) {
+            String k = value.getKey();
+            Object v = value.getValue();
+            if (v != null) {
+                assurePropval(db, recId, assureField(db, k), v, now);
+            }
+        }
+    }
+
+    private static void updatePropval(
+            SQLiteDatabase db,
+            long recId,
+            ContentValues cv,
+            long now)
+    {
+        for (Map.Entry<String,Object> value : cv.valueSet()) {
+            String k = value.getKey();
+            Object v = value.getValue();
+            if (v != null) {
+                assurePropval(db, recId, assureField(db, k), v, now);
+            } else {
+                long fieldId = getField(db, k);
+                if (fieldId > 0) {
+                    deletePropval(db, recId, fieldId);
+                }
+            }
+        }
+    }
+
+    private static void assurePropval(
+            SQLiteDatabase db,
+            long recId,
+            long fieldId,
+            Object value,
+            long now)
+    {
+        // TODO:
+    }
+
+    private static void deletePropval(SQLiteDatabase db, long recId)
+    {
+        // TODO:
+        // should be implemented by trigger?
+    }
+
+    private static void deletePropval(
+            SQLiteDatabase db,
+            long recId,
+            long fieldId)
+    {
+        // TODO:
+    }
+
 
 }
