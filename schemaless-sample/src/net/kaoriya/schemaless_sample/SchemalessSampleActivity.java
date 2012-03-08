6cd89ec2b657ca9be6651f90f7d1280d76f7a407
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-03-08 09:00:34 +0900

    add instant test codes

--- schemaless-sample/src/net/kaoriya/schemaless_sample/SchemalessSampleActivity.java
+++ schemaless-sample/src/net/kaoriya/schemaless_sample/SchemalessSampleActivity.java
@@ -1,13 +1,22 @@
 package net.kaoriya.schemaless_sample;
 
+import java.util.Map;
+
 import android.app.Activity;
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import net.kaoriya.schemaless_database.SchemalessDatabase;
 import net.kaoriya.schemaless_database.SchemalessFactory;
 
-public class SchemalessSampleActivity extends Activity
+public class SchemalessSampleActivity
+    extends Activity
+    implements View.OnClickListener
 {
+    public final static String TAG = "SCHEMALESS";
+
     private SchemalessDatabase database;
 
     /** Called when the activity is first created. */
@@ -20,6 +29,41 @@ public class SchemalessSampleActivity extends Activity
         SchemalessFactory factory = new SchemalessFactory(this,
                 "schemaless.db");
         this.database = factory.newDatabase();
+        findViewById(R.id.DumpCV).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.DumpCV:
+            dumpContentValues();
+            break;
+        }
+    }
+
+    private void dumpContentValues() {
+        Log.v(TAG, "#dumpContentValues");
+        ContentValues cv = new ContentValues();
+        cv.put("byte", Byte.valueOf((byte)0x12));
+        cv.put("int", Integer.valueOf(1234));
+        cv.put("float", Float.valueOf(12.34f));
+        cv.put("short", Short.valueOf((short)1234));
+        cv.put("byte_array", new byte[] { 0, 1, 2, 3, 4 });
+        cv.put("string", "foobar");
+        cv.put("double", Double.valueOf(1.23456));
+        cv.put("long", Long.valueOf(123456789));
+        cv.put("bool", Boolean.TRUE);
+
+        int index = 0;
+        for (Map.Entry<String,Object> value : cv.valueSet()) {
+            String k = value.getKey();
+            Object v = value.getValue();
+            String type = "(null)";
+            if (v != null) {
+                type = v.getClass().getName();
+            }
+            Log.v(TAG, String.format("  #%1$d \"%2$s\" %3$s", index, k, type));
+        }
     }
 
 }
